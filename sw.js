e72444b01de981c5b5c13aed5376ee0bc2390adb
EwakaToram <oktamay23@gmail.com>
2026-07-12 14:12:52 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener(
+"install",
+event => {
+
+self.skipWaiting();
+
+}
+);
+
+self.addEventListener(
+"fetch",
+event => {
+
+event.respondWith(
+fetch(event.request)
+);
+
+}
+);
